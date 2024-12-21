154882b39cfd28b233867547d613d53fec8c35d4
Matt Cholick <cholick@github.com>
2024-12-20 16:22:31 -0800

    Cleanup Tests and Fixing Timing Bug (#1)
    * Don't suppress output
    * Always print command output. Noisy, but don't see a clean way to only output on fail
    * Cleanup running go in tests and add more output
    * Seeing failures in CI only; wait up to a second for messages
    * Exclude docs paths for CI

--- internal/cli/bs_test.go
+++ internal/cli/bs_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cholick/midi-cli/internal/cli"
 	"github.com/cholick/midi-cli/internal/midifakes"
 	"github.com/cholick/midi-cli/internal/ui"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
--- test/e2e/main.py
+++ test/e2e/main.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import time
 import unittest
 
 import mido
@@ -24,135 +25,129 @@ class TestE2E(unittest.TestCase):
         midi_message = mido.Message.from_bytes(message)
         self.messages.append(midi_message)
 
-    def test_port_list(self):
+    def run_go(self, cmd: str):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         go_path = os.path.join(dir_path, "..", "..")
-        result = subprocess.run(
-            "go run cmd/midi-cli/main.go -v port list",
-            shell=True, capture_output=True, cwd=go_path,
-        )
 
-        # todo: is there a way to also print this on any assertion failure?
-        if result.returncode != 0:
-            print("Command stdout")
-            print(decode(result.stdout))
-            print("Command stderr")
-            print(decode(result.stderr))
+        print(f"Running: {cmd}")
+        result = subprocess.run(cmd, shell=True, capture_output=True, cwd=go_path)
+
+        print("Command stdout")
+        print(decode(result.stdout))
+        print("Command stderr")
+        print(decode(result.stderr))
+
+        return result
+
+    def wait_for_message(self, expected: int):
+        count = 0
+        if len(self.messages) < expected and count < 100:
+            time.sleep(.01)
+            count += 1
+
+    def test_port_list(self):
+        cmd = "go run cmd/midi-cli/main.go -v port list"
+        result = self.run_go(cmd)
 
         self.assertEqual(0, result.returncode)
+
         self.assertIn(self.port_name, decode(result.stdout))
 
     def test_send_note_defaults(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        go_path = os.path.join(dir_path, "..", "..")
-        result = subprocess.run(
-            f"go run cmd/midi-cli/main.go -v note on -n c4 --port {self.port_name}",
-            shell=True, capture_output=True, cwd=go_path,
-        )
+        cmd = f"go run cmd/midi-cli/main.go -v note on -n c4 --port {self.port_name}"
+        result = self.run_go(cmd)
 
-        if result.returncode != 0:
-            print("Command stdout")
-            print(decode(result.stdout))
-            print("Command stderr")
-            print(decode(result.stderr))
+        self.assertEqual(0, result.returncode)
 
+        self.wait_for_message(1)
         self.assertEqual(1, len(self.messages))
+
         self.assertEqual('note_on', self.messages[0].type)
         self.assertEqual(127, self.messages[0].velocity)
         self.assertEqual(0, self.messages[0].channel)
         self.assertEqual("C4", pretty_midi.note_number_to_name(self.messages[0].note))
 
     def test_send_note(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        go_path = os.path.join(dir_path, "..", "..")
-        result = subprocess.run(
-            f"go run cmd/midi-cli/main.go -v note on -n d4 -o 121 -c 4 --port {self.port_name}",
-            shell=True, capture_output=True, cwd=go_path,
-        )
+        cmd = f"go run cmd/midi-cli/main.go -v note on -n d4 -o 121 -c 4 --port {self.port_name}"
+        result = self.run_go(cmd)
 
-        if result.returncode != 0:
-            print("Command stdout")
-            print(decode(result.stdout))
-            print("Command stderr")
-            print(decode(result.stderr))
+        self.assertEqual(0, result.returncode)
 
+        self.wait_for_message(1)
         self.assertEqual(1, len(self.messages))
+
         self.assertEqual('note_on', self.messages[0].type)
         self.assertEqual(121, self.messages[0].velocity)
         self.assertEqual(3, self.messages[0].channel)
         self.assertEqual("D4", pretty_midi.note_number_to_name(self.messages[0].note))
 
     def test_program_change(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        go_path = os.path.join(dir_path, "..", "..")
-        result = subprocess.run(
-            f"go run cmd/midi-cli/main.go -v pc -n 4 --port {self.port_name}",
-            shell=True, capture_output=True, cwd=go_path,
-        )
+        cmd = f"go run cmd/midi-cli/main.go -v pc -n 4 --port {self.port_name}"
+        result = self.run_go(cmd)
 
-        if result.returncode != 0:
-            print("Command stdout")
-            print(decode(result.stdout))
-            print("Command stderr")
-            print(decode(result.stderr))
+        print("Command stdout")
+        print(decode(result.stdout))
+        print("Command stderr")
+        print(decode(result.stderr))
 
+        self.assertEqual(0, result.returncode)
+
+        self.wait_for_message(1)
         self.assertEqual(1, len(self.messages))
+
         self.assertEqual('program_change', self.messages[0].type)
         self.assertEqual(4, self.messages[0].program)
 
     def test_control_change(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        go_path = os.path.join(dir_path, "..", "..")
-        result = subprocess.run(
-            f"go run cmd/midi-cli/main.go -v cc -n 3 -l 33 --port {self.port_name}",
-            shell=True, capture_output=True, cwd=go_path,
-        )
+        cmd = f"go run cmd/midi-cli/main.go -v cc -n 3 -l 33 --port {self.port_name}"
+        result = self.run_go(cmd)
 
-        if result.returncode != 0:
-            print("Command stdout")
-            print(decode(result.stdout))
-            print("Command stderr")
-            print(decode(result.stderr))
+        print("Command stdout")
+        print(decode(result.stdout))
+        print("Command stderr")
+        print(decode(result.stderr))
 
+        self.assertEqual(0, result.returncode)
+
+        self.wait_for_message(1)
         self.assertEqual(1, len(self.messages))
+
         self.assertEqual('control_change', self.messages[0].type)
         self.assertEqual(3, self.messages[0].control)
         self.assertEqual(33, self.messages[0].value)
 
     def test_bank_select(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        go_path = os.path.join(dir_path, "..", "..")
-        result = subprocess.run(
-            f"go run cmd/midi-cli/main.go -v bs -l 2 --port {self.port_name}",
-            shell=True, capture_output=True, cwd=go_path,
-        )
+        cmd = f"go run cmd/midi-cli/main.go -v bs -l 2 --port {self.port_name}"
+        result = self.run_go(cmd)
 
-        if result.returncode != 0:
-            print("Command stdout")
-            print(decode(result.stdout))
-            print("Command stderr")
-            print(decode(result.stderr))
+        print("Command stdout")
+        print(decode(result.stdout))
+        print("Command stderr")
+        print(decode(result.stderr))
 
+        self.assertEqual(0, result.returncode)
+
+        self.wait_for_message(1)
         self.assertEqual(1, len(self.messages))
+
         self.assertEqual('control_change', self.messages[0].type)
         self.assertEqual(0, self.messages[0].control)
         self.assertEqual(2, self.messages[0].value)
 
     def test_panic(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        go_path = os.path.join(dir_path, "..", "..")
-        result = subprocess.run(
-            f"go run cmd/midi-cli/main.go -v panic",
-            shell=True, capture_output=True, cwd=go_path,
-        )
+        cmd = f"go run cmd/midi-cli/main.go -v panic"
+        result = self.run_go(cmd)
 
-        if result.returncode != 0:
-            print("Command stdout")
-            print(decode(result.stdout))
-            print("Command stderr")
-            print(decode(result.stderr))
+        print("Command stdout")
+        print(decode(result.stdout))
+        print("Command stderr")
+        print(decode(result.stderr))
 
+        self.assertEqual(0, result.returncode)
+
+        self.wait_for_message(16)
         self.assertEqual(16, len(self.messages))
+
         self.assertEqual('control_change', self.messages[0].type)
         for message in self.messages:
             self.assertEqual(120, message.control)
